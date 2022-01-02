6c94122e5c07101ac6c08f22bd87b7cd7efb4c8f
sergWP <kuzmenko.sergei@gmail.com>
2022-01-02 10:43:46 +0200

    88 lesson

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+const Header = () => {
+    return <h2>Header</h2>
+};
+
+// const Field = () => {
+//     const holder = 'Type';
+//     const styledField = {
+//         width: '300px',
+//         lineHeight: '30px',
+//         paddingLeft: '10px',
+//         paddingRight: '10px'
+//     };
+//     return <input
+//         type="text"
+//         placeholder={holder}
+//         style={styledField}/>
+// }
+
+class Field extends Component {
+    render() {
+        const holder = 'Type';
+        const styledField = {
+            width: '300px',
+            lineHeight: '30px',
+            paddingLeft: '10px',
+            paddingRight: '10px'
+        };
+
+        return <input
+                type="text"
+                placeholder={holder}
+                style={styledField}/>
+    }
+}
+
+function Btn() {
+    const text = 'Start';
+    // const res = () => {
+    //     return 'GO!'
+    // };
+    const p = <span>LogIn</span>;
+    const logged = true;
+    return <button>{logged ? text : p}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,25 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-// const elem = <h2 class='greeting'>Buy Boom!</h2>;
-// const elem = React.createElement('h2', {className: 'greeting'}, 'Buy Boom!');
-
-// многострочная структура в круглых скобках
-// всегда один корневой блок-родитель
-
-const text = 'Buy Boom!';
-const num = 5;
-const elem = (
-    <div>
-      <h2 className={'greeting'}>Buy Boom: {num * 2}</h2>
-      <label htmlFor=""></label>
-      <input type="text" placeholder={'Boom'} />
-      <button tabIndex={0}>Start</button>
-    </div>
-);
+import {Header} from "./App";
 
 ReactDOM.render(
-  elem,
+  <App/>,
   document.getElementById('root')
 );
